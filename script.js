df6dd2f03db3ea95e429aed071b287175c1e37ea
NumanTalukder <51156089+NumanTalukder@users.noreply.github.com>
2023-02-07 19:41:10 +0600

    update

--- script.js
+++ script.js
@@ -1,127 +1,137 @@
-let landLocation = document.getElementById('land-location');
-let landSize = document.getElementById('land-size');
-let pricePerKatha = document.getElementById('price-per-katha');
-let priceLand = document.getElementById('price-whole-land');
-let floorCount = document.getElementById('floor-count');
-let shareCount = document.getElementById('share-count');
-const calulateBtn = document.getElementById('calculate');
-const clearBtn = document.getElementById('clear');
-const infoTable = document.querySelector(".info");
-const costTable = document.querySelector(".cost");
+let landLocation = document.getElementById('land-location')
+let landSize = document.getElementById('land-size')
+let pricePerKatha = document.getElementById('price-per-katha')
+let priceLand = document.getElementById('price-whole-land')
+let floorCount = document.getElementById('floor-count')
+let shareCount = document.getElementById('share-count')
+const calulateBtn = document.getElementById('calculate')
+const clearBtn = document.getElementById('clear')
+const infoTable = document.querySelector('.info')
+const costTable = document.querySelector('.cost')
 const tableRows = document.querySelectorAll('.table-data')
-let serial = tableRows.length;
+let serial = tableRows.length
 
+console.log('Total Common: 1500sqft is divided into each share')
+console.log('Project complition is estimeted to 3 years')
 
-calulateBtn.addEventListener('click', calculate);
-
+calulateBtn.addEventListener('click', calculate)
 
 pricePerKatha.addEventListener('keydown', (e) => {
-    setTimeout(() => {
-        priceLand.value = (landSize.value * e.target.value).toFixed(2);
-        // priceLand.value.toFixed(2);
-    }, 10);
-});
+  setTimeout(() => {
+    priceLand.value = (landSize.value * e.target.value).toFixed(2)
+    // priceLand.value.toFixed(2);
+  }, 10)
+})
 priceLand.addEventListener('keydown', (e) => {
-    setTimeout(() => {
-        pricePerKatha.value = (e.target.value / landSize.value).toFixed(2);
-        // pricePerKatha.value.toFixed(2);
-    }, 10);
-});
-
+  setTimeout(() => {
+    pricePerKatha.value = (e.target.value / landSize.value).toFixed(2)
+    // pricePerKatha.value.toFixed(2);
+  }, 10)
+})
 
 function calculate() {
-    serial++;
-    
-    const infoData = document.createElement('tr');
-    infoData.classList.add('table-data');
-    infoData.classList.add('first-table');
+  serial++
 
-    infoData.innerHTML = `
+  const infoData = document.createElement('tr')
+  infoData.classList.add('table-data')
+  infoData.classList.add('first-table')
+
+  infoData.innerHTML = `
         <td>${serial}</td>
         <td>${landLocation.value}</td>
         <td>${landSize.value}</td>
-        <td>${numberWithCommas(((pricePerKatha.value)/1).toFixed(2))}</td>
-        <td>${numberWithCommas(((priceLand.value)/1).toFixed(2))}</td>
+        <td>${numberWithCommas((pricePerKatha.value / 1).toFixed(2))}</td>
+        <td>${numberWithCommas((priceLand.value / 1).toFixed(2))}</td>
         <td>${floorCount.value}</td>
         <td>${shareCount.value}</td>
         <td>${landSize.value * 3200}</td>
         <td>${((landSize.value * 3200) / shareCount.value).toFixed(2)}</td>
         <!-- common area is 1500 sqft in both ground floor and rooftop -->
-        <td>${((landSize.value * 3200) / shareCount.value).toFixed(2) - (1500 / shareCount.value)}</td>
+        <td>${
+          ((landSize.value * 3200) / shareCount.value).toFixed(2) -
+          1500 / shareCount.value
+        }</td>
     `
 
-    const costData = document.createElement('tr');
-    costData.classList.add('table-data');
-    costData.classList.add('second-table');
+  const costData = document.createElement('tr')
+  costData.classList.add('table-data')
+  costData.classList.add('second-table')
 
-    costData.innerHTML = `
+  costData.innerHTML = `
         <td>${serial}</td>
         <td>2800</td>
-        <td>${numberWithCommas((priceLand.value / shareCount.value).toFixed(2))}</td>
-        <td>${numberWithCommas((((landSize.value * 3200) / shareCount.value).toFixed(2)) * 2800)}</td>
-        <td>${numberWithCommas(((priceLand.value / shareCount.value) + ((((landSize.value * 3200) / shareCount.value)) * 2800)).toFixed(2))}</td>
-        <td>${numberWithCommas((((priceLand.value / shareCount.value) + ((landSize.value * 3200) / shareCount.value) * 2800) / (((landSize.value * 3200) / shareCount.value))).toFixed(2))}</td>
-        <td>${numberWithCommas((priceLand.value / shareCount.value).toFixed(2))}</td>
-        <td>${numberWithCommas(((((((landSize.value * 3200) / shareCount.value).toFixed(2)) * 2800)/36)).toFixed(2))}</td>
+        <td>${numberWithCommas(
+          (priceLand.value / shareCount.value).toFixed(2)
+        )}</td>
+        <td>${numberWithCommas(
+          ((landSize.value * 3200) / shareCount.value).toFixed(2) * 2800
+        )}</td>
+        <td>${numberWithCommas(
+          (
+            priceLand.value / shareCount.value +
+            ((landSize.value * 3200) / shareCount.value) * 2800
+          ).toFixed(2)
+        )}</td>
+        <td>${numberWithCommas(
+          (
+            (priceLand.value / shareCount.value +
+              ((landSize.value * 3200) / shareCount.value) * 2800) /
+            ((landSize.value * 3200) / shareCount.value)
+          ).toFixed(2)
+        )}</td>
+        <td>${numberWithCommas(
+          (priceLand.value / shareCount.value).toFixed(2)
+        )}</td>
+        <td>${numberWithCommas(
+          (
+            (((landSize.value * 3200) / shareCount.value).toFixed(2) * 2800) /
+            36
+          ).toFixed(2)
+        )}</td>
 
     `
 
-    
-    
-    infoTable.appendChild(infoData);
-    costTable.appendChild(costData);
+  infoTable.appendChild(infoData)
+  costTable.appendChild(costData)
 
+  // remove line starts here
 
-    // remove line starts here
+  infoData.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    infoData.remove()
+  })
 
-    infoData.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
-        infoData.remove();
-    });
+  costData.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    costData.remove()
+  })
 
-    costData.addEventListener('contextmenu', (e) => {
-        e.preventDefault();
-        costData.remove();
-    });
+  // remove line ends here
 
-// remove line ends here
+  // active line starts
 
+  infoData.addEventListener('click', () => {
+    infoData.classList.toggle('active')
+  })
+  costData.addEventListener('click', () => {
+    costData.classList.toggle('active')
+  })
 
-// active line starts
-    
-    infoData.addEventListener('click', () => {
-        infoData.classList.toggle('active');
-        
-    });
-    costData.addEventListener('click', () => {
-        costData.classList.toggle('active');
-        
-    });
+  // function removeActive() {
+  //     infoData.classList.remove('active')
+  //     costData.classList.remove('active')
+  // }
 
-    // function removeActive() {
-    //     infoData.classList.remove('active')
-    //     costData.classList.remove('active')
-    // }
-
-// active line starts
-
-
+  // active line starts
 }
 
 //function to sepate the numbers in comma starts
 
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
 //function to sepate the numbers in comma ends
-
-
-
-
-
-
-
 
 // storeData();
 
@@ -136,8 +146,6 @@ function numberWithCommas(x) {
 
 //     // console.log();
 //     localStorage.setItem('table1', JSON.stringify(table1));
-
-
 
 //     secondTable.forEach(table => table2.push(
 //         `
